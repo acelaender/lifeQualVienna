e0b73f7eac9c02ac2e999ded66f2ef3bc76d7aa1
12022509 <acelaender@gmail.com>
2025-10-25 12:13:27 +0200

    init

---
+++ src/main/java/at/fhtw/lifequalvienna/LifeQualViennaApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.lifequalvienna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeQualViennaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeQualViennaApplication.class, args);
+	}
+
+}
